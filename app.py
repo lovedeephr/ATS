ed8e9b3400fa5e55ce280bd2c689bb5925238d47
lovedeephr <45961154+lovedeephr@users.noreply.github.com>
2024-11-22 23:17:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+# In-memory database for now (we'll improve this later)
+job_postings = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/post_job', methods=['POST'])
+def post_job():
+    title = request.form['title']
+    description = request.form['description']
+    job_postings.append({'title': title, 'description': description})
+    return "Job posted successfully!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
